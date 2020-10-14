beb9c498d3eafb4eca801d6fe5e762ca877171be
csesese <63036918+csesese@users.noreply.github.com>
2020-10-14 19:00:15 +0900

    201014 python 1day

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+#실수형 더 알아보기
+a=0.3+0.9
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+if a==0.9:
+    print(True)
+else:
+    print(False)
+
+#round 반올림
+print(round(0.123923, 3))
+
+#리스트 자료형 : 여러개의 데이터를 연속적ㅇ로 담아 처리하기 위해 사용하는 자료형
+#리스트 컴프리헨션 : 리스트를 초기화 하는 방법 중 하나 , 대괄호 안에 조건문과 반복문을 적용하여 리스트를 초기화 할 수 있음
+
+#0부터 9까지의 수를 포함하는 리스트
+a=[i for i in range(10)]
+print(a)
+
+#1부터 9까지의 수들의 제곱 값을 포함하는 리스트
+b=[i * i for i in range(10)]
+print(b)
+
+#5번 출력하기(언더바)
+for _ in range(5):
+    print("hello")
+
+#리스트에서 특정 값을 가지는 원소를 모두 제거하기
+a=[1,2,3,4,5,5,5]
+remove_set = {3,5}
+
+result = [i for i in a if i not in remove_set]
+print(result)
+
+#------------------------------------------------------------
+
+#문자열 자료형  : 문자열 변수를 초기화 할때는 큰따옴표 나 작은따옴표를 이용합니다.
+
+data = 'hello world'
+print(data)
+# " 큰따옴표를 출력하기 위해서는 \ 뒤에 작성
+data ="Dont you know  \"Python\" ?"
+print(data)
+
+#문자열 연산
+
+a="String"
+print(a*3)
+
+a="abcdef"
+print(a[2:4])
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
